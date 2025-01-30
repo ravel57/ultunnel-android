23f0930abfbc7e23d27b6a73ddb4178d79fb8829
petr <petyalom@yandex.ru>
2025-01-31 01:23:43 +0300

    added downloading configs from server by secret key

--- app/src/main/java/io/nekohasekai/sfa/bg/ProxyService.kt
+++ app/src/main/java/io/nekohasekai/sfa/bg/ProxyService.kt
@@ -5,16 +5,24 @@ import android.content.Intent
 import io.nekohasekai.libbox.Notification
 import io.nekohasekai.libbox.TunOptions
 
-class ProxyService :
-    Service(),
-    PlatformInterfaceWrapper {
+class ProxyService : Service(), PlatformInterfaceWrapper {
+
     private val service = BoxService(this, this)
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int) = service.onStartCommand()
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int) =
+        service.onStartCommand()
 
     override fun onBind(intent: Intent) = service.onBind()
 
     override fun onDestroy() = service.onDestroy()
 
-    override fun sendNotification(notification: Notification) = service.sendNotification(notification)
+    override fun writeLog(message: String) = service.writeLog(message)
+
+    override fun sendNotification(notification: Notification) =
+        service.sendNotification(notification)
+
+    override fun updateRouteOptions(options: TunOptions?) {
+        TODO("Not yet implemented")
+    }
+
 }
